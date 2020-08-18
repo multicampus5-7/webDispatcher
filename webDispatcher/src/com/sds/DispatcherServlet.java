40e09bc3e998db0102887d7d6f729639d6b16a40
kcloud721 <kcloud721@naver.com>
2020-08-18 13:27:52 +0900

    index

---
+++ webDispatcher/src/com/sds/DispatcherServlet.java
@@ -0,0 +1,48 @@
+package com.sds;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet({ "/DispatcherServlet", "/dispatcher" })
+public class DispatcherServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public DispatcherServlet() {
+        super();
+    }
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		request.setCharacterEncoding("EUC-KR");
+		
+		String uri = request.getRequestURI();
+		String path = uri.substring(uri.lastIndexOf("/"));
+		path = path.substring(1, path.lastIndexOf("."));
+		System.out.println("request:"+path);
+		String next = "main.jsp";
+		if(path.equals("main")) {
+			next = "main.jsp";
+		}else if(path.equals("login")) {
+			next = "main.jsp";
+			request.setAttribute("centerpage", "login");
+		}else {
+			next = path;
+		}
+		RequestDispatcher rd = 
+		request.getRequestDispatcher(next);
+		rd.forward(request, response);
+
+	}
+
+}
+
+
+
+
+
---
+++ webDispatcher/web/posui.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+    pageEncoding="EUC-KR"%>    
+    
+<div id="center">
+<h1>POS SYSTEM</h1>
+<div id="product"></div>
+<h2>TOTAL:<span id="total"></span></h2>
+<div id="cart"></div>
+<h2>CHARGE:<span id="status"></span></h2>
+<div id="charge">
+	<button id="pay">PAY</button>
+	<button id="admin">ADMIN</button>
+	<button id="reset">RESET</button>
+</div>
+<h2>RECEIPT:</h2>
+<div id="receipt" style="width: 100%; height: auto; border: 2px solid gray;"></div>
+<div id="map" style="width: 100%; height: 600px; border: 2px solid blue;"></div>
+</div>
